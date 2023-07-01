91fcd656089017056024614c10950a03f2adc7ae
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2023-07-01 08:03:47 +0000

    i still making this lift

--- defaultinput.sh
+++ defaultinput.sh
@@ -5,6 +5,37 @@
 SOURCE='YOUR_INPUT_SERVER_HERE' # can be an video file
 RTMP='YOUR_SERVER_STREAMING_HERE'
 KEY='YOUR_KEY_HERE'
+default_config='config.json' # must be json
+function setup (){
+	if [[ "$(find $default_config)" != "$default_config" ]];
+		echo "prepare setup"
+	else
+		exit 0;
+	fi
+}
+function help (){
+	echo '''List of arguments:
+-f Default file source (required)
+-s Streaming sources (required)
+-k Streaming key (optional)'''
+}
+while getopts 'f:s:k:h' option; do
+	case $option in 
+		f) SOURCE=$OPTARG;;
+		s) RTMP=$OPTARG;;
+		k) KEY=$OPTARG;;
+		h) help;;
+		\?) echo "invaild"; exit 1;;
+
+	esac
+done
+function main (){
+if [[ $@ == "" ]]; then
+	help; exit 1
+fi
+echo "SOURCE: $SOURCE"
+echo "RTMP: $RTMP"
+echo "KEY: $KEY"
 ffmpeg \
 	-re \
 	-i $SOURCE \
@@ -16,3 +47,5 @@ ffmpeg \
 	-flvflags no_duration_filesize \
 	-c copy \
 	-f flv $RTMP/$KEY
+}
+main "$@"
