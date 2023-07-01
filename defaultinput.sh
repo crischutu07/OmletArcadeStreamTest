2fb6683b6b98a77d1afbcb2282183cb6bc8c2c5d
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2023-07-01 20:16:34 +0000

    Added getopts arguments, setup and configuration

--- defaultinput.sh
+++ defaultinput.sh
@@ -20,34 +20,43 @@ function yaml {
       }
    }'
 }
-
-if [[ "$(find $default_config &> /dev/null; echo "$?")" != "0" ]]; then
-	setup
-fi
-
-function setup (){
-echo '''
-file_path='/path/to/file.mp4' // can be a link source
-rtmp_source='rtmp://example.com/stream'
-rtmp_key='rtmp_key'
-''' > $default_config
-echo "setup is done, please now run ./defaultinput.sh"
-}
 function help (){
 	echo """List of arguments:
 -f Default file source (required)
 -s Streaming sources (required)
 -k Streaming key (optional)"""
 }
-#while getopts 'f:s:k:h' option; do
-#	case $option in 
-#		f) SOURCE=$OPTARG;;
-#		s) RTMP=$OPTARG;;
-#		k) KEY=$OPTARG;;
-#		h) help;;
-#		\?) echo "invaild"; exit 1;;
-#	esac
-#done
+if [[ "$(find $default_config &> /dev/null; echo "$?")" != "0" ]]; then
+   echo "Let's get started!"
+   read -p "What path do you want to set as a video? (could be a link source/required): " SOURCE
+   if [[ "$SOURCE" == "" ]]; then
+      echo "Video path/source required, please try again"
+      exit 1
+   fi
+   read -p "Please provide streaming service also? (requied/must run RTMP): rtmp://" SRTMP
+   if [[ "$RTMP" == "" ]]; then
+      echo "Streaming server required, please try again"
+      exit 1
+   fi
+   read -p "Please provide with a stream key (optional): " KEY
+echo """
+file_path: '$(pwd)/$SOURCE' # can be a link source
+rtmp_source: '$RTMP'
+rtmp_key: '$KEY'
+""" > $default_config
+echo "setup is done, please now run ./defaultinput.sh"
+exit 0
+fi
+
+while getopts 'f:s:k:h' option; do
+	case $option in 
+		f) SOURCE=$OPTARG;;
+		k) KEY=$OPTARG;;
+		s) RTMP=$OPTARG;;
+		h) help;;
+		\?) echo "invaild"; exit 1;;
+	esac
+done
 function main (){
 if [[ "$*" == "" ]]; then
 	help; exit 1
@@ -67,5 +76,4 @@ ffmpeg \
 	-c copy \
 	-f flv $RTMP/$KEY
 }
-cat $default_config | yaml rtmp_key
 main "$@"
